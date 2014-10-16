7e3eaa8aab1064acd38453dc735fba2a93ca0739
od107 <od107@users.noreply.github.com>
2014-10-16 15:41:56 +0200

    insert & contains first attempt

--- KdTrees/src/KdTree.java
+++ KdTrees/src/KdTree.java
@@ -1,25 +1,125 @@
 
 public class KdTree {
+	   private Node root;
+	
+	   private static class Node {
+		   private Point2D p;      // the point
+		   private RectHV rect;    // the axis-aligned rectangle corresponding to this node
+		   private Node lb;        // the left/bottom subtree
+		   private Node rt;        // the right/top subtree
+		   
+		   private Node(Point2D p) {
+		   	this.p = p;
+		   	lb = null;
+		   	rt = null;
+		   }
+/*		   
+		    private Node(Node n, Point2D p) {
+		   	this.p = p;
+		   	lb = null;
+		   	rt = null;
+		   }
+*/	   }
+
 	   public KdTree() {
 		   // construct an empty set of points 
-		   
+		   root = new Node();
 	   }
 	   public boolean isEmpty() {
 		   // is the set empty? 
-		   
+		   return root == null;
 	   }
 	   public int size()  {
 		   // number of points in the set 
-		   
+		   int count = 0;
+		   return subsize(root, count);
 	   }
-	   public void insert(Point2D p) {
+	   
+	   private int subsize(Node n, int count) {
+		   while (n != null) {
+		   	if(n.lb != null) {
+		   		count = count + subsize(n.lb, count);
+		   	}
+		   	if(n.rt != null) {
+		   		count = count + subsize(n.rt, count);
+		   	}
+		   }
+		   return count;
+	   }
+
+	   private void insert(Point2D p) {
 		   // add the point to the set (if it is not already in the set)
-		   
+		assert !contains(p);
+		
+	   	Node n = root;
+	   	boolean lr = false;
+	   	boolean islb;
+	   	Node parent;
+	   	
+	   	while (n != null) {
+			lr = !lr;
+	   		if(lr) {
+	   			if (p.x < n.p.x) { //check syntax
+	   				islb = true;
+	   				parent = n;
+	   				n = n.lb
+	   			}
+	   			else {
+	   				islb = false
+	   				;parent = n;
+	   				n = n.rt;
+	   			}
+	   				
+	   		}
+	   		else {
+	   			if (p.y < n.p.y) {
+	   				islb = true;
+	   				parent = n;
+					n = n.lb
+	   			}
+	   			else {
+	   				islb = false;
+	   				parent = n;
+	   				n = n.rt;
+	   			}
+	   		}
+	   	}
+	   	
+	   	Node newNode = new Node(p);
+	   	if(islb)
+	   		parent.lb = newNode;
+	   	else
+	   		parent.rt = newNode;
+	   	
+	   	assert contains(p);
 	   }
+	   
 	   public  boolean contains(Point2D p) {
 		   // does the set contain point p? 
-		   
+		   Node n = root;
+		   boolean lr = true;
+		   return search(n, p, lr);
 	   }
+	   
+	   private boolean search(Node n, Point2D p, boolean lr) {
+	   	if (n == null)
+	   	   return false;
+	   	if (n.p.equals(p))
+		   return true;
+		if (lr) {
+		   if (p.x < n.p.x)
+		   	search (n.lb, p, !lr);
+		   else
+			search (n.rt, p, !lr); 
+		}
+		else {
+			if (p.y < n.p.y)
+				search (n.lb, p, !lr);
+		 	else
+				search (n.rt, p, !lr); 
+		}
+	   }
+	   
 	   public void draw() {
 		   // draw all points to standard draw 
 		   
