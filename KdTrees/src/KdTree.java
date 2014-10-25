43491a5dbdca3e19f55338784af4b914532b26b3
Jochen Deferme <jochen.deferme@gmail.com>
2014-10-25 19:29:10 +0200

    first solution, some trouble with contains()

--- KdTrees/src/KdTree.java
+++ KdTrees/src/KdTree.java
@@ -1,24 +1,33 @@
+import java.util.ArrayList;
+
 
 public class KdTree {
 	private Node root;
 
 	private static class Node {
+		// should we store also the lr <> tb in the node? 
+		
 		private Point2D p;      // the point
 		private RectHV rect;    // the axis-aligned rectangle corresponding to this node
 		private Node lb;        // the left/bottom subtree
 		private Node rt;        // the right/top subtree
+//		private boolean lr; // true = left/right, false  = top/down
 
 		private Node(Point2D p) {
 			this.p = p;
-			lb = null;
-			rt = null;
 		}
+		
+		private Node(Point2D p, RectHV rect) {
+			this.p = p;
+			this.rect = rect;
+		}
+		
 	}
 
 	public KdTree() {
 		// construct an empty set of points 
 		//this will not work for multiple instances
-		root = null;
+		root = null; //new Node();
 	}
 	public boolean isEmpty() {
 		// is the set empty? 
@@ -45,48 +54,71 @@ public class KdTree {
 	public void insert(Point2D p) {
 		// add the point to the set (if it is not already in the set)
 		assert !contains(p);
-
-		Node n = root;
-		boolean lr = false;
-		boolean islb = false;
-		Node parent = null;
-
-		while (n != null) {
-			lr = !lr;
-			if(lr) {
-				if (p.x() < n.p.x()) { 
-					islb = true;
-					parent = n;
-					n = n.lb;
-				}
-				else {
-					islb = false
-							;parent = n;
-							n = n.rt;
-				}
-
-			}
-			else {
-				if (p.y() < n.p.y()) {
-					islb = true;
-					parent = n;
-					n = n.lb;
-				}
-				else {
-					islb = false;
-					parent = n;
-					n = n.rt;
-				}
-			}
+		
+		if (root == null) {
+			root = new Node(p, new RectHV(0,0,1,1));
 		}
+		else {
+			Node n = root;
+			boolean lr = false; //true = left/right, false = top / bottom
+			boolean islb = false;//true = left half of search tree, false = right half
+			Node parent = null;
 
-		Node newNode = new Node(p);
-		if(islb)
-			parent.lb = newNode;
-		else
-			parent.rt = newNode;
+			//this could be done more elegantly using again recursion
+			while (n != null) {
+				lr = !lr; //every time we go a level deeper we change the lr/tb division
+				if(lr) {
+					if (p.x() < n.p.x()) { 
+						islb = true;
+						parent = n;
+						n = n.lb;
+					}
+					else {
+						islb = false;
+						parent = n;
+						n = n.rt;
+					}
 
+				}
+				else {
+					if (p.y() < n.p.y()) {
+						islb = true;
+						parent = n;
+						n = n.lb;
+					}
+					else {
+						islb = false;
+						parent = n;
+						n = n.rt;
+					}
+				}
+			}
+			
+			Node newNode = new Node(p, calcRect(parent, islb, lr));			
+			if(islb) {
+				parent.lb = newNode;
+			}
+			else
+				parent.rt = newNode;
+		}
 		assert contains(p);
+	}
+	
+	private RectHV calcRect(Node node, boolean islb, boolean lr) {
+		RectHV rect;
+		if(lr){
+			if(islb)
+				rect = new RectHV(node.rect.xmin(),node.rect.ymin(),node.p.x(),node.rect.ymax());
+			else
+				rect = new RectHV(node.p.x(),node.rect.ymin(),node.rect.xmax(),node.rect.ymax());
+		}
+		else { // : top/bottom
+			if(islb)
+				rect = new RectHV(node.rect.xmin(),node.rect.ymin(),node.rect.xmax(),node.p.y());
+			else
+				rect = new RectHV(node.rect.xmin(),node.p.y(),node.rect.xmax(),node.rect.ymax());
+		}
+		return rect;
 	}
 
 	public  boolean contains(Point2D p) {
@@ -118,15 +150,74 @@ public class KdTree {
 
 	public void draw() {
 		// draw all points to standard draw 
-
+		Node n = root;
+		draw(n);
 	}
+	
+	private void draw(Node n) {
+		while (n != null) {
+			n.p.draw();
+			draw(n.lb);
+			draw(n.rt);
+		}
+	}
+	
 	public Iterable<Point2D> range(RectHV rect) {
 		// all points that are inside the rectangle 
-
+		if(isEmpty())
+			return null;
+		
+		// TODO
+		ArrayList<Point2D> list = new ArrayList<Point2D>();
+		boolean lr = false;
+		list = contains(list, rect, root, lr);
+		
+		return list;
 	}
+	
+	private ArrayList<Point2D> contains(ArrayList<Point2D> list, RectHV rect, Node n, boolean lr){
+		if (rect.contains(n.p))
+			list.add(n.p);
+		if(lr) {
+			if (n.lb != null && n.p.x() > rect.xmin()) //rect to the left, search left searchtree
+				list = contains(list, rect, n.lb, !lr);
+			if (n.rt != null && n.p.x() < rect.xmax()) // search right subtree
+				list = contains(list, rect, n.rt, !lr);
+		}
+		else {
+			if (n.rt != null && n.p.y() > rect.ymin())
+				list = contains(list, rect, n.rt, !lr);
+			if (n.lb != null && n.p.y() < rect.ymax())
+				list = contains(list, rect, n.lb, !lr);
+		}
+		return list;
+	}
+	
+	
 	public Point2D nearest(Point2D p) {
 		// a nearest neighbor in the set to point p; null if the set is empty 
-
+		if(isEmpty())
+			return null;
+		
+		return nearest(root, p, root.p);
+	}
+	
+	private Point2D nearest(Node node, Point2D p , Point2D nearest) {
+		//TODO in order for this to work the rectangle needs to work
+		
+		if(node.p.distanceSquaredTo(p) < nearest.distanceSquaredTo(p))	
+			//If the distance from the node point is smaller than nearest found
+			nearest = node.p;
+		
+		if(node.lb != null && node.lb.rect.distanceSquaredTo(p) < node.p.distanceSquaredTo(p)){	
+			//If the distance from the nodes left child rect is smaller
+    		nearest = nearest(node.lb, p, nearest);
+    	}
+    	if(node.rt != null && node.rt.rect.distanceSquaredTo(p) < node.p.distanceSquaredTo(p))	
+    		// if the distance from the nodes right child rect is smaller
+    		nearest = nearest(node.rt, p , nearest);
+    	
+    	return nearest;
 	}
 
 	public static void main(String[] args) {
