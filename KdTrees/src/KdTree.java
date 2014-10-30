9a39105fc65ab9e28f54240ce901ebc642f5ab25
Jochen Deferme <jochen.deferme@gmail.com>
2014-10-30 21:09:00 +0100

    completed draw, removed bug from contains

--- KdTrees/src/KdTree.java
+++ KdTrees/src/KdTree.java
@@ -6,13 +6,10 @@ public class KdTree {
 	private Node root;
 
 	private static class Node {
-		// should we store also the lr <> tb in the node? 
-		
 		private Point2D p;      // the point
 		private RectHV rect;    // the axis-aligned rectangle corresponding to this node
 		private Node lb;        // the left/bottom subtree
 		private Node rt;        // the right/top subtree
-//		private boolean lr; // true = left/right, false  = top/down
 
 		private Node(Point2D p) {
 			this.p = p;
@@ -22,13 +19,11 @@ public class KdTree {
 			this.p = p;
 			this.rect = rect;
 		}
-		
 	}
 
 	public KdTree() {
 		// construct an empty set of points 
-		//this will not work for multiple instances
-		root = null; //new Node();
+		root = null; 
 	}
 	public boolean isEmpty() {
 		// is the set empty? 
@@ -80,7 +75,6 @@ public class KdTree {
 						parent = n;
 						n = n.rt;
 					}
-
 				}
 				else {
 					if (p.y() < n.p.y()) {
@@ -149,19 +143,42 @@ public class KdTree {
 
 	public void draw() {
 		// draw all points to standard draw 
-		Node n = root;
-		draw(n);
+		draw(root, true);
+        StdDraw.setPenColor(StdDraw.BLACK);
+        root.rect.draw();
 	}
 	
-	private void draw(Node n) {
+	private void draw(Node n, boolean lr) {
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(.01);
 		n.p.draw();
-		n.rect.draw();
-		if(n.lb != null) {
-			draw(n.lb);
-		}
+		StdDraw.setPenRadius(.001);
+		
+		if(n.lb != null) 
+			draw(n.lb, !lr);
+
 		if(n.rt != null) {
-			draw(n.rt);
-		}				
+			draw(n.rt, !lr);
+		}	
+
+		if(n.lb == null && n.rt == null) {
+			if(lr){
+				//draw the final lines (bottom nodes do not have child rect)
+				StdDraw.setPenColor(StdDraw.RED);
+				n.p.drawTo(new Point2D(n.p.x(),n.rect.ymax()));
+				n.p.drawTo(new Point2D(n.p.x(),n.rect.ymin()));
+			}
+			else {
+				StdDraw.setPenColor(StdDraw.BLUE);
+				n.p.drawTo(new Point2D(n.rect.xmax(), n.p.y()));
+				n.p.drawTo(new Point2D(n.rect.xmin(), n.p.y()));
+			}
+		}
+		if(!lr)
+			StdDraw.setPenColor(StdDraw.RED);
+		else
+			StdDraw.setPenColor(StdDraw.BLUE);
+		n.rect.draw();	
 	}
 	
 	public Iterable<Point2D> range(RectHV rect) { // more correct to use a set
@@ -186,10 +203,10 @@ public class KdTree {
 				list = contains(list, rect, n.rt, !lr);
 		}
 		else {
-			if (n.rt != null && n.p.y() > rect.ymin())
-				list = contains(list, rect, n.rt, !lr);
-			if (n.lb != null && n.p.y() < rect.ymax())
+			if (n.lb != null && n.p.y() > rect.ymin())
 				list = contains(list, rect, n.lb, !lr);
+			if (n.rt != null && n.p.y() < rect.ymax())
+				list = contains(list, rect, n.rt, !lr);
 		}
 		return list;
 	}
@@ -205,16 +222,15 @@ public class KdTree {
 	
 	private Point2D nearest(Node node, Point2D p , Point2D nearest) {
 		//TODO in order for this to work the rectangle needs to work
-		
 		if(node.p.distanceSquaredTo(p) < nearest.distanceSquaredTo(p))	
 			//If the distance from the node point is smaller than nearest found
 			nearest = node.p;
 		
-		if(node.lb != null && node.lb.rect.distanceSquaredTo(p) < node.p.distanceSquaredTo(p)){	
+		if(node.lb != null && node.lb.rect.distanceSquaredTo(p) < nearest.distanceSquaredTo(p)) //node.p.distanceSquaredTo(p)){	
 			//If the distance from the nodes left child rect is smaller
     		nearest = nearest(node.lb, p, nearest);
-    	}
-    	if(node.rt != null && node.rt.rect.distanceSquaredTo(p) < node.p.distanceSquaredTo(p))	
+    	
+    	if(node.rt != null && node.rt.rect.distanceSquaredTo(p) < nearest.distanceSquaredTo(p)) //node.p.distanceSquaredTo(p))	
     		// if the distance from the nodes right child rect is smaller
     		nearest = nearest(node.rt, p , nearest);
     	
@@ -229,31 +245,54 @@ public class KdTree {
 	
 	private static RectHV randomRect() {
 		double x1 = StdRandom.uniform();
-		double x2 = StdRandom.uniform(x1,1);
+		double x2 = StdRandom.uniform(); 
+		if(x1 > x2){
+			double swap = x1;
+			x1 = x2;
+			x2 = swap;
+		}
 		double y1 = StdRandom.uniform();
-		double y2 = StdRandom.uniform(y1,1);
-		
+		double y2 = StdRandom.uniform();
+		if(y1 > y2){
+			double swap = y1;
+			y1 = y2;
+			y2 = swap;
+		}		
 		return new RectHV(x1, y1, x2, y2);
 	}
-	
 
 	public static void main(String[] args) {
 		// unit testing of the methods (optional) 
-		int nbr = 1000;
-		
+		boolean fileinput = true;
+        String filename = "testing\\input5.txt";
+		int nbr = 10;
+       	
 		KdTree kd = new KdTree();
 		PointSET set = new PointSET();
 		TreeSet<Point2D> reference = new TreeSet<Point2D>();
 		
 		if(!set.isEmpty() || !kd.isEmpty())
 			System.out.println("Problem in the empty() method");
-		
-		for (int i=0;i<nbr;i++) {
-			Point2D point = randomPoint();
-			kd.insert(point);
-			set.insert(point);
-			reference.add(point);
-		}
+        
+        if(fileinput) {
+            In in = new In(filename);
+        	while (!in.isEmpty()) {
+        		double x = in.readDouble();
+        		double y = in.readDouble();
+        		Point2D p = new Point2D(x, y);
+        		kd.insert(p);
+        		set.insert(p);
+        		reference.add(p);
+        	}
+        }
+        else { // add a number of random points
+        	for (int i=0;i<nbr;i++) {
+        		Point2D point = randomPoint();
+        		kd.insert(point);
+        		set.insert(point);
+        		reference.add(point);
+        	}
+        }
 		
 		if(kd.size() == set.size())
 			System.out.println("sizes match");
@@ -262,23 +301,29 @@ public class KdTree {
 			System.out.println("size set: " + set.size());
 		}
 		
-		for (Point2D p : reference) { // problem in insert or in contains?
+		for (Point2D p : reference) { 
 			if (!set.contains(p))
 				System.out.println("point: " + p + " is not contained in the set");
 			if(!kd.contains(p))
 				System.out.println("point: " + p + " is not contained in the kdtree");
 		}
 		
+//		kd.draw();
+		
+//		kd.nearest(new Point2D(.81,.30));
+		
 		for (int i=0 ; i<100 ; i++) {
 			Point2D p = randomPoint();
-			if(set.nearest(p) != kd.nearest(p))
+			if(!set.nearest(p).equals(kd.nearest(p)))
 				System.out.println("problem in the nearest() method");
+			p.draw();
 		}
 		
 		for (int i=0 ; i<100 ; i++) {
 			RectHV rect = randomRect();
 			Iterable<Point2D> setContained = set.range(rect);
 			Iterable<Point2D> kdContained = kd.range(rect);
+			rect.draw();
 			for (Point2D point : setContained)
 				if(!kd.contains(point))
 					System.out.println("point: " + point + " exist in set but not in kd");
